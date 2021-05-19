e3a62eabc9d8962b36db5aa35ab431165c319592
maqhb <mqbutt@live.com>
2021-05-19 10:36:26 +0500

    realtime chat

---
+++ livestream/utils/logger.js
@@ -0,0 +1,45 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, label, prettyPrint } = format;
+
+var transportsLogger = [];
+
+transportsLogger.push(
+    new transports.Console({
+        level: 'info',
+        format: format.combine(
+            format.colorize(),
+            format.simple()
+        )
+    })
+)
+
+// transportsLogger.push(
+//     new transports.DailyRotateFile({
+//         level: process.env.ENV === 'apidevelopment' ? 'debug' : 'info',
+//         datePattern: 'YYYYMMDD',
+//         filename: 'public/logs/user%DATE%.log',
+//         handleExceptions: true,
+//         json: true,
+//         maxSize: '1g',
+//         maxFiles: '3d'
+//     })
+// );
+
+
+var logger = createLogger({
+    format: combine(
+        timestamp(),
+        prettyPrint()
+    ),
+    transports: transportsLogger,
+    exitOnError: false
+});
+
+logger.stream = {
+    write: function(message, encoding){
+        logger.info(message);
+    }
+};
+
+module.exports = logger;
+
